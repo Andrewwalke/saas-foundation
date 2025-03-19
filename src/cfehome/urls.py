ae6ff7b9f734d7fc60514ef86022bfc18ebc9ca9
Andrew <coderandrew1@gmail.com>
2025-03-19 07:24:09 +0530

    prepared for railway

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+
+from .views import home_view, about_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',views.home)
+    path("", home_view), #index page -> root page
+    path("about/", about_view),
+    path("hello-world/", home_view),
+    path("hello-world.html", home_view),
+    path("admin/", admin.site.urls),
 ]
